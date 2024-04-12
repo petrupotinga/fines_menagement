1a72b4bdf1ebff1ded397d6bad580edefee22099
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-04-12 11:42:31 +0300

    proiect

---
+++ src/test/java/com/potinga/springboot/fines_menagement/FinesMenagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potinga.springboot.fines_menagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinesMenagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
