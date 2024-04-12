280aa5d02a982382d733415d511384b294a27b15
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-04-12 11:36:13 +0300

    Create FinesMenagementApplication.java

---
+++ src/main/java/com/potinga/springboot/fines_menagement/FinesMenagementApplication.java
@@ -0,0 +1,13 @@
+package com.potinga.springboot.fines_menagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinesMenagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinesMenagementApplication.class, args);
+	}
+
+}
