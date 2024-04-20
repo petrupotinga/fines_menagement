6be61d64d7e408c6ee570bee85ec630acf5d4ae8
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-04-20 15:26:30 +0300

    Am modificat CreateVehicleRequest

--- src/main/java/com/potinga/springboot/fines_menagement/dto/rest/vehicle/CreateVehicleRequest.java
+++ src/main/java/com/potinga/springboot/fines_menagement/dto/rest/vehicle/CreateVehicleRequest.java
@@ -1,9 +1,16 @@
 package com.potinga.springboot.fines_menagement.dto.rest.vehicle;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class CreateVehicleRequest {
     private String vin;
     private String licensePlate;
-    private int ownerId;
+    private Long ownerId;
     private String make;
     private String model;
     private int year;
@@ -11,7 +18,7 @@ public class CreateVehicleRequest {
     public CreateVehicleRequest() {
     }
 
-    public CreateVehicleRequest(String vin, String licensePlate, int ownerId, String make, String model, int year) {
+    public CreateVehicleRequest(String vin, String licensePlate, Long ownerId, String make, String model, int year) {
         this.vin = vin;
         this.licensePlate = licensePlate;
         this.ownerId = ownerId;
@@ -20,51 +27,5 @@ public class CreateVehicleRequest {
         this.year = year;
     }
 
-    public String getVin() {
-        return vin;
-    }
 
-    public void setVin(String vin) {
-        this.vin = vin;
-    }
-
-    public String getLicensePlate() {
-        return licensePlate;
-    }
-
-    public void setLicensePlate(String licensePlate) {
-        this.licensePlate = licensePlate;
-    }
-
-    public int getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(int ownerId) {
-        this.ownerId = ownerId;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
 }
