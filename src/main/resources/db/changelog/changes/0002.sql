19b5e3ea7a2c67e32a8e17daac7adecae3c8c939
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-04-21 13:50:34 +0300

    Create 0002.sql

---
+++ src/main/resources/db/changelog/changes/0002.sql
@@ -0,0 +1,7 @@
+CREATE TABLE owner (
+         id SERIAL PRIMARY KEY,
+         first_name VARCHAR(255) NOT NULL,
+         last_name VARCHAR(255) NOT NULL,
+         address VARCHAR(255) NOT NULL,
+         phone_number VARCHAR(255) NOT NULL
+);
