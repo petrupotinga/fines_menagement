a24c1f4ccf86dfc98e8c170304674efb888e60aa
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-04-26 17:48:02 +0300

    Create VehicleController.java

---
+++ src/main/java/com/potinga/springboot/fines_menagement/controller/VehicleController.java
@@ -0,0 +1,24 @@
+package com.potinga.springboot.fines_menagement.controller;
+
+import com.potinga.springboot.fines_menagement.dto.rest.vehicle.CreateVehicleRequest;
+import com.potinga.springboot.fines_menagement.dto.rest.vehicle.VehicleCreatedResponse;
+import com.potinga.springboot.fines_menagement.service.VehicleService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/vehicle")
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @PostMapping
+    public VehicleCreatedResponse createVehicle(CreateVehicleRequest request) {
+        VehicleCreatedResponse response = vehicleService.createVehicle(request);
+        return response;
+    }
+}
