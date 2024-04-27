d0d6b27cf5c24a0c10fce8f95a6fc98a1fbad69b
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-04-27 11:02:27 +0300

    Create CreateFineReguest.java

---
+++ src/main/java/com/potinga/springboot/fines_menagement/dto/rest/fine/CreateFineReguest.java
@@ -0,0 +1,31 @@
+package com.potinga.springboot.fines_menagement.dto.rest.fine;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CreateFineReguest {
+    private double amount;
+    private String violation;
+    private String date;
+    private String location;
+    private Long ownerId;
+    private Long vehicleId;
+
+    public CreateFineReguest(double amount, String violation, String date, String location, Long ownerId, Long vehicleId) {
+        this.amount = amount;
+        this.violation = violation;
+        this.date = date;
+        this.location = location;
+        this.ownerId = ownerId;
+        this.vehicleId = vehicleId;
+    }
+}
+
+
+
+
+
