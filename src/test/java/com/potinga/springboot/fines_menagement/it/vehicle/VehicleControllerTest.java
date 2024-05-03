dae1f28b4d71b80cd5acbdfd7ec260f5bfd51e7e
Artur Frimu <artur.frimu2@gmail.com>
2024-05-03 15:22:09 +0300

    Am fixat testul care preia toate vehiculele din baza

--- src/test/java/com/potinga/springboot/fines_menagement/it/vehicle/VehicleControllerTest.java
+++ src/test/java/com/potinga/springboot/fines_menagement/it/vehicle/VehicleControllerTest.java
@@ -2,34 +2,26 @@ package com.potinga.springboot.fines_menagement.it.vehicle;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.potinga.springboot.fines_menagement.apiclient.VehicleApiClient;
-import com.potinga.springboot.fines_menagement.common.PostgreSQLContainerInitializer;
 import com.potinga.springboot.fines_menagement.common.PostgresIntegrationTest;
 import com.potinga.springboot.fines_menagement.dto.rest.vehicle.AllVehicleResponse;
 import com.potinga.springboot.fines_menagement.dto.rest.vehicle.CreateVehicleRequest;
 import com.potinga.springboot.fines_menagement.dto.rest.vehicle.VehicleByIdResponse;
 import com.potinga.springboot.fines_menagement.dto.rest.vehicle.VehicleCreatedResponse;
 import com.potinga.springboot.fines_menagement.entity.OwnerEntity;
+import com.potinga.springboot.fines_menagement.entity.VehicleEntity;
 import com.potinga.springboot.fines_menagement.repository.OwnerRepository;
+import com.potinga.springboot.fines_menagement.repository.VehicleRepository;
 import com.potinga.springboot.fines_menagement.utils.JsonReader;
 import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.RequestEntity;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @PostgresIntegrationTest
 class VehicleControllerTest {
@@ -41,6 +33,8 @@ class VehicleControllerTest {
     private VehicleApiClient vehicleApiClient;
     @Autowired
     private OwnerRepository ownerRepository;
+    @Autowired
+    private VehicleRepository vehicleRepository;
 
     @Test
     @DisplayName("Create a new vehicle")
@@ -49,7 +43,7 @@ class VehicleControllerTest {
         ownerEntity.setFirstName(RandomStringUtils.randomAlphabetic(10));
         ownerEntity.setLastName(RandomStringUtils.randomAlphabetic(10));
         ownerEntity.setAddress(RandomStringUtils.randomAlphabetic(10));
-        ownerEntity.setPhoneNumber(RandomStringUtils.randomAlphabetic(10));
+        ownerEntity.setPhoneNumber(RandomStringUtils.randomNumeric(10));
         OwnerEntity savedOwner = ownerRepository.save(ownerEntity);
 
         //        GIVEN
@@ -72,6 +66,31 @@ class VehicleControllerTest {
     @Test
     @DisplayName("Get all vehicles")
     void getAllVehiclesTest() {
+        OwnerEntity ownerEntity = new OwnerEntity();
+        ownerEntity.setFirstName(RandomStringUtils.randomAlphabetic(10));
+        ownerEntity.setLastName(RandomStringUtils.randomAlphabetic(10));
+        ownerEntity.setAddress(RandomStringUtils.randomAlphabetic(10));
+        ownerEntity.setPhoneNumber(RandomStringUtils.randomNumeric(10));
+        OwnerEntity savedOwner = ownerRepository.save(ownerEntity);
+
+        VehicleEntity vehicle1 = new VehicleEntity();
+        vehicle1.setMake("Dacia");
+        vehicle1.setModel("Logan");
+        vehicle1.setVin("XMCLNDABXHY329876");
+        vehicle1.setYear(2016);
+        vehicle1.setLicensePlate("DCC220");
+        vehicle1.setOwner(savedOwner);
+
+        VehicleEntity vehicle2 = new VehicleEntity();
+        vehicle2.setMake("Mercedes");
+        vehicle2.setModel("E220");
+        vehicle2.setVin("PSALNDABXHY329712");
+        vehicle2.setYear(2018);
+        vehicle2.setLicensePlate("ACD321");
+        vehicle2.setOwner(savedOwner);
+
+        vehicleRepository.saveAll(List.of(vehicle1, vehicle2));
+
         //        GIVEN
         List<AllVehicleResponse> allVehicleResponses = JsonReader.read("db/mocks/vehicles/allVehicles.json", ALL_VEHICLES_TYPE_REFERENCE);
 
