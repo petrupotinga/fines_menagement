b39539c6b41010be3d4fe31e1ee18dca720ea412
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-05-14 18:25:13 +0300

    Update VehicleService.java

--- src/main/java/com/potinga/springboot/fines_menagement/service/VehicleService.java
+++ src/main/java/com/potinga/springboot/fines_menagement/service/VehicleService.java
@@ -1,15 +1,18 @@
 package com.potinga.springboot.fines_menagement.service;
 
+import com.potinga.springboot.fines_menagement.dto.rest.vehicle.AllVehicleResponse;
 import com.potinga.springboot.fines_menagement.dto.rest.vehicle.CreateVehicleRequest;
+import com.potinga.springboot.fines_menagement.dto.rest.vehicle.VehicleByLPResponse;
 import com.potinga.springboot.fines_menagement.dto.rest.vehicle.VehicleCreatedResponse;
-import com.potinga.springboot.fines_menagement.dto.rest.vehicle.VehicleByIdResponse;
 import com.potinga.springboot.fines_menagement.entity.OwnerEntity;
 import com.potinga.springboot.fines_menagement.entity.VehicleEntity;
 import com.potinga.springboot.fines_menagement.repository.OwnerRepository;
 import com.potinga.springboot.fines_menagement.repository.VehicleRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class VehicleService {
@@ -46,12 +49,42 @@ public class VehicleService {
 
         return response;
     }
-
-    public VehicleByIdResponse getVehicleById(Long id) {
-        Optional<VehicleEntity> optionalVehicle = vehicleRepository.findById(id);
+    //    public VehicleByIdResponse getVehicleById(Long id) {
+//        Optional<VehicleEntity> optionalVehicle = vehicleRepository.findById(id);
+//        if (optionalVehicle.isPresent()) {
+//            VehicleEntity vehicle = optionalVehicle.get();
+//            VehicleByIdResponse response = new VehicleByIdResponse();
+//            response.setId(vehicle.getId());
+//            response.setMake(vehicle.getMake());
+//            response.setModel(vehicle.getModel());
+//            response.setVin(vehicle.getVin());
+//            response.setYear(vehicle.getYear());
+//            response.setLicensePlate(vehicle.getLicensePlate());
+//            response.setOwnerId(vehicle.getOwner().getId());
+//            return response;
+//        } else {
+//            throw new RuntimeException("Vehicle not found for id: " + id);
+//        }
+//    }
+    public List<AllVehicleResponse> getAllVehicles() {
+        List<VehicleEntity> vehicles = vehicleRepository.findAll();
+        return vehicles.stream().map(vehicle -> {
+            AllVehicleResponse response = new AllVehicleResponse();
+            response.setId(vehicle.getId());
+            response.setMake(vehicle.getMake());
+            response.setModel(vehicle.getModel());
+            response.setVin(vehicle.getVin());
+            response.setYear(vehicle.getYear());
+            response.setLicensePlate(vehicle.getLicensePlate());
+            response.setOwnerId(vehicle.getOwner().getId());
+            return response;
+        }).collect(Collectors.toList());
+    }
+    public VehicleByLPResponse getVehicleByLicensePlate(String licensePlate) {
+        Optional<VehicleEntity> optionalVehicle = vehicleRepository.findByLicensePlate(licensePlate);
         if (optionalVehicle.isPresent()) {
             VehicleEntity vehicle = optionalVehicle.get();
-            VehicleByIdResponse response = new VehicleByIdResponse();
+            VehicleByLPResponse response = new VehicleByLPResponse();
             response.setId(vehicle.getId());
             response.setMake(vehicle.getMake());
             response.setModel(vehicle.getModel());
@@ -61,7 +94,7 @@ public class VehicleService {
             response.setOwnerId(vehicle.getOwner().getId());
             return response;
         } else {
-            throw new RuntimeException("Vehicle not found for id: " + id);
+            throw new RuntimeException("Vehicle with license plate " + licensePlate + " not found");
         }
     }
 }
