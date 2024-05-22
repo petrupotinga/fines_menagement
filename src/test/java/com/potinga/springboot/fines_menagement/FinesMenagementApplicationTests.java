e57ad545ced651f1e853d931c69ef08b6e85fc48
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-05-22 20:48:22 +0300

    Am fixat FinesMenagementApplicationTests

--- src/test/java/com/potinga/springboot/fines_menagement/FinesMenagementApplicationTests.java
+++ src/test/java/com/potinga/springboot/fines_menagement/FinesMenagementApplicationTests.java
@@ -1,13 +1,21 @@
 package com.potinga.springboot.fines_menagement;
 
+import com.potinga.springboot.fines_menagement.common.PostgresIntegrationTest;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@PostgresIntegrationTest
+@RequiredArgsConstructor
 class FinesMenagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final ApplicationContext applicationContext;
 
+    @Test
+    void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+    }
 }
