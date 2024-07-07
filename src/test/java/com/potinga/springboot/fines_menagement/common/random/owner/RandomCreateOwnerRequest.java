dbf8f08fe3f331fdbbfd30ba102e40e74e2915ad
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-07-07 20:26:24 +0200

    Create RandomCreateOwnerRequest.java

---
+++ src/test/java/com/potinga/springboot/fines_menagement/common/random/owner/RandomCreateOwnerRequest.java
@@ -0,0 +1,24 @@
+package com.potinga.springboot.fines_menagement.common.random.owner;
+
+import com.potinga.springboot.fines_menagement.dto.rest.owner.CreateOwnerRequest;
+import lombok.Builder;
+import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.function.Supplier;
+
+@Builder
+public class RandomCreateOwnerRequest implements Supplier<CreateOwnerRequest> {
+    @Builder.Default
+    private final String firstName = RandomStringUtils.randomAlphabetic(20);
+    @Builder.Default
+    private final String lastName = RandomStringUtils.randomAlphabetic(20);
+    @Builder.Default
+    private final String address = RandomStringUtils.randomAlphabetic(20);
+    @Builder.Default
+    private final String phoneNumber = RandomStringUtils.randomAlphabetic(20);
+
+    @Override
+    public CreateOwnerRequest get() {
+        return new CreateOwnerRequest(firstName, lastName, address, phoneNumber);
+    }
+}
