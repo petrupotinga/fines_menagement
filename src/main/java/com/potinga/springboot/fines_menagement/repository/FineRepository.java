c538d5b17017fe622bd737a4ade36f25f4b2ea45
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-07-27 21:25:50 +0200

    Am adaugat findAllByVehicleLicensePlate()- FineRepository

--- src/main/java/com/potinga/springboot/fines_menagement/repository/FineRepository.java
+++ src/main/java/com/potinga/springboot/fines_menagement/repository/FineRepository.java
@@ -1,11 +1,16 @@
 package com.potinga.springboot.fines_menagement.repository;
 
 import com.potinga.springboot.fines_menagement.entity.FineEntity;
+import com.potinga.springboot.fines_menagement.entity.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FineRepository extends JpaRepository<FineEntity, Long> {
 
     List<FineEntity> findByVehicleId(Long id);
+    @Query("SELECT f FROM FineEntity f WHERE f.vehicle.licensePlate = :licensePlate")
+    List<FineEntity> findAllByVehicleLicensePlate(@Param("licensePlate") String licensePlate);
 }
