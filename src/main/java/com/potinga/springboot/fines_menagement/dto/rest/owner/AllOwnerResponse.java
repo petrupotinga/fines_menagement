7184b03a529630e418aa8c70a1b9cb7380b7e081
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-08-10 09:56:52 +0200

    Update AllOwnerResponse.java

--- src/main/java/com/potinga/springboot/fines_menagement/dto/rest/owner/AllOwnerResponse.java
+++ src/main/java/com/potinga/springboot/fines_menagement/dto/rest/owner/AllOwnerResponse.java
@@ -14,21 +14,24 @@ public class AllOwnerResponse {
     private String idnp;
     private String firstName;
     private String lastName;
+    private String birthDate;
     private String address;
     private String phoneNumber;
-    public AllOwnerResponse(String idnp, String firstName, String lastName, String address, String phoneNumber) {
+    public AllOwnerResponse(String idnp, String firstName, String lastName, String birthDate, String address, String phoneNumber) {
         this.idnp = idnp;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.birthDate = birthDate;
         this.address = address;
         this.phoneNumber = phoneNumber;
     }
 
-    public AllOwnerResponse(Long id, String idnp, String firstName, String lastName, String address, String phoneNumber) {
+    public AllOwnerResponse(Long id, String idnp, String firstName, String lastName,String birthDate, String address, String phoneNumber) {
         this.id = id;
         this.idnp = idnp;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.birthDate = birthDate;
         this.address = address;
         this.phoneNumber = phoneNumber;
     }
